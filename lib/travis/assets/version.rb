c11adc66062492f41ac4f59e835d324fe2736163
Sven Fuchs <svenfuchs@artweb-design.de>
2012-05-03 15:59:16 +0200

    recompile

---
+++ lib/travis/assets/version.rb
@@ -0,0 +1,35 @@
+require 'digest/md5'
+
+module Travis
+  module Assets
+    class Version
+      FILE = 'public/current'
+
+      attr_reader :root
+
+      def initialize(root)
+        @root = root
+        write
+      end
+
+      def version
+        @version ||= digest.to_s[0..7]
+      end
+
+      protected
+
+        def file
+          @version_file ||= root.join(FILE)
+        end
+
+        def write
+          file.open('w+') { |f| f.write(version) }
+          self
+        end
+
+        def digest
+          Digest::MD5.new << `ls -alR AssetFile Gemfile.lock #{root}/assets`
+        end
+    end
+  end
+end
